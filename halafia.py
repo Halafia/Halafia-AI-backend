209ed373e57d4e104d967b61ffd42595b90ea0c2
aerialmagic <habibelediko@gmail.com>
2023-12-01 08:28:03 +0000

    chatGPT custom data with ajax web example

--- halafia.py
+++ halafia.py
@@ -1,2 +1,27 @@
-API_KEY = open("API_KEY", "r").read()
+import os
+from langchain.document_loaders import TextLoader
+from langchain.document_loaders import DirectoryLoader
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.llms import openai
+from langchain.chat_models import ChatOpenAI
+
+
+os.environ["OPENAI_API_KEY"] = open("API_KEY", "r").read()
+
+def generateResponse(prompt):
+    query = prompt
+
+
+    #loader = TextLoader('custom_data.txt')
+    loader = DirectoryLoader(path="./Data", glob = "*.txt")
+
+    index = VectorstoreIndexCreator().from_loaders([loader])
+
+    try:
+        response = index.query(query, llm=ChatOpenAI(max_tokens=100, temperature=1)).replace('\n', '<br>')
+
+    except:
+        response = 'oops, You beat the AI'
+
+    return response
 
--- main.py
+++ main.py
@@ -1,22 +1,40 @@
-import sys
+from flask import Flask, render_template, jsonify, request
 import halafia
-import os
-
-os.environ["OPENAI_API_KEY"] = halafia.API_KEY
-
-from langchain.document_loaders import TextLoader
-from langchain.document_loaders import DirectoryLoader
-from langchain.indexes import VectorstoreIndexCreator
-from langchain.llms import openai
-from langchain.chat_models import ChatOpenAI
 
 
-query = input('user: ')
+
+def page_not_found():
+    return render_template('404.html'), 404
+
+app = Flask(__name__)
+app.config.from_mapping(
+    SECRET_KEY='HALAFIA')
+
+app.register_error_handler(404, page_not_found)
+
+@app.route('/', methods = ['POST', 'GET'])
+def index():
+
+    if request.method == 'POST':
+        prompt = request.form['prompt']
+
+        res = {}
+        res['response'] = halafia.generateResponse(prompt)
+        return jsonify(res), 200
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        port='5000', 
+        debug=True
+    )
 
 
-#loader = TextLoader('custom_data.txt')
-loader = DirectoryLoader(path="./Data", glob = "*.txt")
-
-index = VectorstoreIndexCreator().from_loaders([loader])
-print(index.query(query, llm=ChatOpenAI()))
-
+#@halafia.route('/create file', methods= ['POST'])
+#def create_file():
+#    if request.method == 'POST':
+#        with open(f"{request.form.get('name')}.txt", "w") as f:
+#            f.write()
+#        return('', 204)
